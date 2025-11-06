a706f20811722f5a95de262adf428785e7c63361
tusharshukla34 <tusharshukla880@gmail.com>
2025-11-06 09:30:18 +0530

    analyzing

--- job application tracker.sql
+++ job application tracker.sql
@@ -106,30 +106,63 @@ select * from interviews;
 
 -- 1. Total number of applications by status (Applied, Interview, Rejected, Selected).
 
+select status, count(*) as Count_of_status from applications group by status;
+
 
 -- 2. Applications per month (trend over time).
+
+select date_format(date_applied , '%Y-%M') as month, count(app_id) as Total_application_per_month from applications
+ group by month order by min(date_applied);
 
 
 -------------------------------------------------- Company Analysis
 
 -- 3. Top 5 companies with the highest number of applications.
 
+select company_name, count(app_id) as Number_of_application from applications
+ group by company_name order by Number_of_application desc limit 5 ;
 
 -- 4. Rejection rate per company (who rejects the most?).
+
+select company_name,count(*) as Number_of_rejected, 
+ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM applications WHERE company_name = a.company_name), 2) AS Rejection_Rate_Percent 
+from applications a where status = 'rejected' 
+group by company_name order by Number_of_rejected desc;
 
 
 -------------------------------------------------- Applicant Analysis
 
 -- 5. Total applications submitted per applicant.
 
+select a.name, count(*) as number_of_applications from applicants a join applications app on a.applicant_id = app.applicant_id
+group by a.name;
+
 
 -- 6. Selection rate per applicant (who’s doing best?).
+
+SELECT 
+    a.name,
+    COUNT(CASE WHEN app.status = 'selected' THEN 1 END) AS selected_count,
+    COUNT(*) AS total_applications,
+    ROUND(
+        COUNT(CASE WHEN app.status = 'selected' THEN 1 END) * 100.0 / COUNT(*),
+        2
+    ) AS selection_rate_percent
+FROM applicants a
+JOIN applications app ON a.applicant_id = app.applicant_id
+GROUP BY a.name
+ORDER BY selection_rate_percent DESC;
 
 
 ----------------------------------------------------- Role Insights
 
 -- 7. Top 5 most applied job roles.
 
+select job_role,count(*) as Number_of_application from applications group by job_role order by Number_of_application desc limit 5;
 
 -- 8. Status distribution per role (Applied, Interview, Rejected, Selected).
 
+
+select job_role, status, count(*) AS count_per_status from applications
+group by job_role, status
+order by job_role, count_per_status DESC;
